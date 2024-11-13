44d65d941328fd44e2a2693f2cefecfb01491248
kbgbak <k.b.gunn.bak@gmail.com>
2024-11-13 11:27:02 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_5CdO9rqKH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4m5ecjgp5sgis3j56llhl9i3on', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
